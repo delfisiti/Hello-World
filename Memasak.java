b8505fa256d831b603417e5f436ad5db2551ddae
delfisiti <32594155+delfisiti@users.noreply.github.com>
2017-10-18 19:16:15 +0700

    Add files via upload

---
+++ Memasak.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memasak;
+
+/**
+ *
+ * @author twin girl's
+ */
+public class Memasak {
+
+    public static void main(String[] args) {
+        System.out.println("TUGAS OOP");
+        System.out.println("Siti Delfi Nurhuda");
+        System.out.println("");
+        System.out.println("Cara Memasak Nasi di Rice Cooker :");
+        System.out.println("1. Cuci beras yang telah ditakar sebelumnya. Untuk rice cooker kecil biasanya hanya bisa memuat 0,6 L beras");
+        System.out.println("2. Masukkan beras ke dalam inner pan atau panci rice cooker kecil. Pastikan inner pan dalam keadaan bersih sebelum digunakan. Kebersihan tersebut dapat mempengaruhi kelezatan nasi yang dihasilkan.");
+        System.out.println("3. Masukkan air ke dalam inner pan sesuai dengan banyaknya beras yang telah dimasukkan sebelumnya");
+        System.out.println("4. Langkah selanjutnya adalah memasukkan inner pan ke dalam body rice cooker. Kita dapat memutar inner pan akan posisinya tepat berada di atas lempeng pemanas atau heating plate. Kita juga harus memastikan bahwa lempeng tersebut dalam keadaan bersih dan tidak ada benda yang menghalangi antara inner pan dengan heating plate.");
+        System.out.println("5. Langkah berikutnya yaitu menutup body rice cooker menggunakan top cover hingga clamb knop mengeluarkan suara ‘klik’.");
+        System.out.println("6. Setelah itu, kita harus menyambungkan rice cooker ke sumber listrik dengan cara memasukkan steker ke stop kontak.");
+        System.out.println("7. Langkah terakhir adalah menekan level knob ke arah bawah hingga lampu indikator cooking mode atau mode memasak menyala. Kita bisa menunggu hingga nasi matang yaitu ketika lampu indikator berubah menjadi warm mode. Warna lampu indikator ini berbeda- beda pada setiap rice cooker, tergantung pada mereknya.");
+    }
+    
+}
